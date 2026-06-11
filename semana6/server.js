6d63d79fc2bfd631943fa9b04b928932dfbf6c03
Hugoabello2-dev <hugoabello2@gmail.com>
2026-06-10 21:41:20 -0500

    Actividad semana 6

--- semana6/server.js
+++ semana6/server.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/registro', (req, res) => {
+
   const nombre = req.body.nombre;
   const mensaje = req.body.mensaje;
 
@@ -12,25 +13,9 @@ app.post('/registro', (req, res) => {
     nombre: nombre,
     mensaje: mensaje
   });
+
 });
 
-app.post('/incidencia', (req, res) => {
-  const tipo = req.body.tipo;
-  const descripcion = req.body.descripcion;
-
-  res.json({
-    mensaje: "Incidencia registrada",
-    tipo: tipo,
-    descripcion: descripcion
-  });
-});
-
-// AGREGA ESTO
-app.get('/prueba', (req, res) => {
-  res.send('Ruta de prueba funcionando');
-});
-
-// YA LO TENÍAS
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
